37ddbb4703141b1486ecde0953b74b30d2429da6
tbanerj2 <114112752+tbanerj2@users.noreply.github.com>
2022-11-14 16:42:43 -0500

    Add files via upload

---
+++ Hand_tracking_angles.py
@@ -0,0 +1,109 @@
+import mediapipe as mp
+import cv2
+import numpy as np
+import time
+sTime=0
+eTime=0
+mp_drawing = mp.solutions.drawing_utils
+mp_hands = mp.solutions.hands
+mp_hands.HandLandmark.WRIST
+
+joint_list = [[8,7,6], [7,6,5], [6,5,0]]
+def get_label(index, hand, results):
+    output = None
+    for idx, classification in enumerate(results.multi_handedness):
+        if classification.classification[0].index == index:
+            # Process results
+            label = classification.classification[0].label
+            text = '{}'.format(label)
+
+            # Extract Coordinates
+            coords = tuple(np.multiply(
+                np.array((hand.landmark[mp_hands.HandLandmark.WRIST].x, hand.landmark[mp_hands.HandLandmark.WRIST].y)),
+                [640, 480]).astype(int))
+
+            output = text, coords
+
+    return output
+
+
+def draw_finger_angles(image, results, joint_list):
+    # Loop through hands
+    for hand in results.multi_hand_landmarks:
+        # Loop through joint sets
+        for joint in joint_list:
+            a = np.array([hand.landmark[joint[0]].x, hand.landmark[joint[0]].y])  # First coord
+            b = np.array([hand.landmark[joint[1]].x, hand.landmark[joint[1]].y])  # Second coord
+            c = np.array([hand.landmark[joint[2]].x, hand.landmark[joint[2]].y])  # Third coord
+
+            radians = np.arctan2(c[1] - b[1], c[0] - b[0]) - np.arctan2(a[1] - b[1], a[0] - b[0])
+            angle = np.abs(radians * 180.0 / np.pi)
+
+            if angle > 180.0:
+                angle = 360 - angle
+
+            cv2.putText(image, str(round(angle, 2)),tuple(np.multiply(b, [640, 480]).astype(int)),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+
+
+    return image
+
+cap = cv2.VideoCapture(0)
+
+sTime=time.time()
+with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5) as hands:
+    while cap.isOpened():
+        ret, frame = cap.read()
+
+        # BGR 2 RGB
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+        # Flip on horizontal
+        image = cv2.flip(image, 1)
+
+        # Set flag
+        image.flags.writeable = False
+
+        # Detections
+        results = hands.process(image)
+
+        # Set flag to true
+        image.flags.writeable = True
+
+        # RGB 2 BGR
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+        # Detections
+        print(results)
+
+        # Rendering results
+        if results.multi_hand_landmarks:
+            for num, hand in enumerate(results.multi_hand_landmarks):
+                mp_drawing.draw_landmarks(image, hand, mp_hands.HAND_CONNECTIONS,
+                                          mp_drawing.DrawingSpec(color=(121, 0, 76), thickness=2, circle_radius=4),
+                                          mp_drawing.DrawingSpec(color=(250, 0, 250), thickness=2, circle_radius=4),
+                                          )
+
+                # Render left or right detection
+                if get_label(num, hand, results):
+                    text, coord = get_label(num, hand, results)
+                    cv2.putText(image, text, coord, cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+
+            draw_finger_angles(image, results, joint_list)
+        # Finding the frame capture rate
+        eTime = time.time()
+        fps = 1 / (eTime - sTime)
+        sTime=eTime
+        cv2.putText(image, f'FPS:{int(fps)}', (20, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
+        cv2.imshow('Hands Mediapipe', image)
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+
+cap.release()
+cv2.destroyAllWindows()
+
+
+
+
+
+
